ad99ef9c20eef975346fe520401bf18c13f4acec
= <=>
2024-04-10 08:41:02 -0300

    first commit

---
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddWebApiConfig(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWebApiConfig(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors("Development");
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
